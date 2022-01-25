ff96662582dd6c8f067154b0dc6c186ab805cb56
fangkuai767 <67937954+fangkuai767@users.noreply.github.com>
2022-01-25 19:49:17 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -296,10 +296,10 @@ function showGameScoreLayer() {
     l.className = l.className.replace(/bgc\d/, 'bgc' + c);
     document.getElementById('GameScoreLayer-text').innerHTML = shareText(_gameScore);
     let score_text = '得分&nbsp;&nbsp;';
-    score_text += deviation_time < 23000 ? _gameScore : "<span style='color:red;'>" + _gameScore + "</span>";
+    score_text += deviation_time < 9999999999999999999999 ? _gameScore : "<span style='color:red;'>" + _gameScore + "</span>";
     document.getElementById('GameScoreLayer-score').innerHTML = score_text;
     let bast = cookie('bast-score');
-    if (deviation_time < 23000) {
+    if (deviation_time < 9999999999999999999999) {
         if (!bast || _gameScore > bast) {
             bast = _gameScore;
             cookie('bast-score', bast, 100);
