46f5a35bcd01a9a178dfc2a4da36c06cb9f44cd7
fangkuai767 <67937954+fangkuai767@users.noreply.github.com>
2022-01-25 19:15:42 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -174,17 +174,7 @@ function SubmitResults() {
     }
 }
 
-function gameTime() {
-    _gameTimeNum--;
-    if (_gameTimeNum <= 0) {
-        GameTimeLayer.innerHTML = '&nbsp;&nbsp;&nbsp;&nbsp;时间到！';
-        gameOver();
-        GameLayerBG.className += ' flash';
-        createjs.Sound.play("end");
-    } else {
-        GameTimeLayer.innerHTML = creatTimeText(_gameTimeNum);
-    }
-}
+
 
 function creatTimeText(n) {
     return '&nbsp;TIME:' + n;
